e38b9d3e0d9df250020044fad97433f77d056803
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-22 18:09:00 +0100

    fix route path

--- config/express.js
+++ config/express.js
@@ -4,7 +4,7 @@ module.exports = function () {
   var app = express(),
       bodyParser = require('body-parser'),
       config = require('./config'),
-      router = require('.././app/user/routes/gateway.route');
+      router = require('../app/gateway/routes/gateway.route');
 
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
