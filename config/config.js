221fd42276ed9c9023bcd1dc81b6f06a37d6a9d2
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-22 12:40:57 +0100

    write skeletal config file

---
+++ config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  route: {
+    development: {
+      
+      url: ''
+    },
+    test: {
+
+      url: ''
+    },
+    production: {
+      url: ''
+    }
+  },
+
+  port: process.env.PORT || 5555
+
+};
