f2f398eb5a87e3cca9ee4ba157a5e457558b1bec
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-23 15:11:46 +0100

    code clean up

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var config = require('./config/config'),
     app = require('./config/express')();
 
-console.log(config.route[process.env.NODE_ENV].users.url + "/login");
 
 app.listen(config.port, function (){
   console.log("Server started on PORT: " + config.port);
